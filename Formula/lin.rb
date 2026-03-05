8c275c6acf47ee9c65891109adae08e2584b13f0
Ian <75198764+6missedcalls@users.noreply.github.com>
2026-03-04 23:16:21 -0800

    chore: bump lin to v0.3.1

--- Formula/lin.rb
+++ Formula/lin.rb
@@ -1,22 +1,22 @@
 class Lin < Formula
   desc "Fast, terminal-native client for Linear"
   homepage "https://github.com/6missedcalls/lin-cli"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/6missedcalls/lin-cli/releases/download/v0.3.0/lin-darwin-arm64.tar.gz"
-      sha256 "c59f8aed9ee0b80d62ba9a43395233897dd36acb43a7684fa77c93b68a9ff2b6"
+      url "https://github.com/6missedcalls/lin-cli/releases/download/v0.3.1/lin-darwin-arm64.tar.gz"
+      sha256 "3928dff45e959c196e579cf3adb582c44e357a6ed84af57ba9fa93a034b3b38e"
     else
-      url "https://github.com/6missedcalls/lin-cli/releases/download/v0.3.0/lin-darwin-x86_64.tar.gz"
-      sha256 "f897f0abfdbeffe2cd738b305463bf95229fae6f9243aa3922a120551679b0af"
+      url "https://github.com/6missedcalls/lin-cli/releases/download/v0.3.1/lin-darwin-x86_64.tar.gz"
+      sha256 "1ff3af912f9b2e28025705ef3aa3a8fdf6bcf465852cbebb8811263f1b2b8dfb"
     end
   end
 
   on_linux do
-    url "https://github.com/6missedcalls/lin-cli/releases/download/v0.3.0/lin-linux-x86_64.tar.gz"
-    sha256 "a96d1bf092617c3034a06b110f57a84b469ade6f497e331ae65c8045e8033e0c"
+    url "https://github.com/6missedcalls/lin-cli/releases/download/v0.3.1/lin-linux-x86_64.tar.gz"
+    sha256 "f655fa43ad0d010f99ec7b32e5b7025c753760ea4934355b54a8588bdaf7602f"
   end
 
   def install
@@ -24,6 +24,6 @@ class Lin < Formula
   end
 
   test do
-    assert_match "v0.3.0", shell_output("#{bin}/lin --version")
+    assert_match "v0.3.1", shell_output("#{bin}/lin --version")
   end
 end
